73fb92dc564ff5a5dcf6dfc5dc7b5f40821aaa8f
Leonardo Lima <leo.sopranzetti@hotmail.com>
2021-05-21 16:16:41 -0300

    axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router' 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import http from '@/http'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+const app = createApp({})
+app.config.globalProperties.$http = http
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(VueAxios, axios).mount('#app')
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -6,11 +6,13 @@
             <div class="row mb-3 mt-3">
                     <div class="form-group col">
                         <label for="exampleInputEmail1"><h5>Primeira cidade</h5> </label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Primeira cidade">
+                        <input  class="form-control mb-2" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Primeira cidade" v-model="city.name">
+                        <button @click.prevent="check1()" type="submit" class="btn btn-primary ">Disponibilidade</button>
                     </div>
                     <div class="form-group col">
                         <label for="exampleInputPassword1"><h5>Segunda Cidade</h5></label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Segunda cidade">
+                        <input  class="form-control mb-2" id="exampleInputPassword1" placeholder="Segunda cidade" v-model="city2.name">
+                        <button @click.prevent="check2()" type="submit" class="btn btn-primary ">Disponibilidade</button>
                     </div>
                 
             </div>
@@ -23,10 +25,54 @@
 </template>
 
 <script>
+
     export default {
-        name: 'home'
+        name: 'home',
+
+    data(){
+    return{
+        city:{
+            id:'',
+            name:'',
+            uf: '',
+            ibge:'',
+            geolocation:'',
+            location:''
+        },
+        city2:{
+            id:'',
+            name:'',
+            uf: '',
+            ibge:'',
+            geolocation:'',
+            location:''
+        }
+    }
+    },
+
+    methods: {
+        check1() {
+        this.$http     
+            .get("http://localhost:8080/cities/" + this.city.name)
+            .then(resposta => {
+                this.city = resposta.data
+                console.log(resposta.data)
+                })
+            .catch(erro => console.log(erro))
+        },
+        check2() {
+        this.$http
+            .get("http://localhost:8080/cities/" + this.city2.name)
+            .then(resposta => {
+                this.city2 = resposta.data
+                console.log(resposta)
+                })
+            .catch(erro => console.log(erro))
+        }
+  }
+    }
   
-}
+
 
 </script>
 
