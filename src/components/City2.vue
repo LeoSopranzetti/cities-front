e13a1b3b49109f81bbc4c6aeec421761612ed956
Leonardo Lima <leo.sopranzetti@hotmail.com>
2021-05-24 16:09:02 -0300

    novos components

---
+++ src/components/City2.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="card" v-if="verdadeiro()">
+				<div class="card-header alert-dark" style="background-color: #d2cfcf"><h4>{{city2.name}}</h4></div>
+				
+				<div class="card-body">
+					<div class="row">
+						<div class="col-12 col-sm-6  mb-3">
+							<dl class="row">		
+							<dt class="col-sm-5">Estado:</dt> <dd class="col-sm-7 ">{{city2.uf.name}}</dd>				
+							<dt class="col-sm-5">IBGE:</dt> <dd class="col-sm-7 "> {{city2.ibge}}</dd>						
+							
+							</dl>
+						</div>
+					</div>
+				</div>
+			</div>
+</template>
+
+<script>
+export default {
+    props: ['city2'],
+
+    data() {
+        return {
+			
+        }
+    },
+
+    methods: {
+        verdadeiro(){
+            
+            if(this.city2.ibge === ''){
+                return false 
+            } else {
+                return true
+            }
+        }
+    }
+}
+</script>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -6,12 +6,15 @@
             <div class="row mb-3 mt-3">
                     <div class="form-group col">
                         <label for="exampleInputEmail1"><h5>Primeira cidade</h5> </label>
-                        <input  class="form-control mb-2" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Primeira cidade" v-model="city.name">
+                        <input  class="form-control mb-2" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Primeira cidade" v-model="cidadeNome">
+                        <City v-model="city" :city="city" class="mb-3"/>
+                        
                         <button @click.prevent="check1()" type="submit" class="btn btn-primary ">Disponibilidade</button>
                     </div>
                     <div class="form-group col">
                         <label for="exampleInputPassword1"><h5>Segunda Cidade</h5></label>
-                        <input  class="form-control mb-2" id="exampleInputPassword1" placeholder="Segunda cidade" v-model="city2.name">
+                        <input  class="form-control mb-2" id="exampleInputPassword1" placeholder="Segunda cidade" v-model="cidade2Nome">
+                        <City2 v-model="city2" :city2="city2" class="mb-3"/>
                         <button @click.prevent="check2()" type="submit" class="btn btn-primary ">Disponibilidade</button>
                     </div>
                 
@@ -25,9 +28,14 @@
 </template>
 
 <script>
-
+import City from '@/components/City'
+import City2 from '@/components/City2'
     export default {
         name: 'home',
+
+        components: {
+          City, City2
+        },
 
     data(){
     return{
@@ -46,14 +54,16 @@
             ibge:'',
             geolocation:'',
             location:''
-        }
+        },
+        cidadeNome: '',
+        cidade2Nome: ''
     }
     },
 
     methods: {
         check1() {
         this.$http     
-            .get("http://localhost:8080/cities/" + this.city.name)
+            .get("http://localhost:8080/cities/" + this.cidadeNome)
             .then(resposta => {
                 this.city = resposta.data
                 console.log(resposta.data)
@@ -62,7 +72,7 @@
         },
         check2() {
         this.$http
-            .get("http://localhost:8080/cities/" + this.city2.name)
+            .get("http://localhost:8080/cities/" + this.cidade2Nome)
             .then(resposta => {
                 this.city2 = resposta.data
                 console.log(resposta)
